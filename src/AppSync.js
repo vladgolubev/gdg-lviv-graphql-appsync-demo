cb9bd1335f3e3f7b154703d391b5531d1729ec0e
Vlad Holubiev <golubev.vld@gmail.com>
2018-06-26 16:05:10 +0300

    chore: include AppSync.js to git

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+  "graphqlEndpoint": "https://vyc2k6jl5bapjn27tkbc6x4vey.appsync-api.us-east-1.amazonaws.com/graphql",
+  "region": "us-east-1",
+  "authenticationType": "API_KEY",
+  "apiKey": "da2-hghcmjpa75hxvbuq4uuzxgbcrq"
+}
